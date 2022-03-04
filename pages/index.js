aae5cad79ec802ca9a4bf2729a76cc29f3dec183
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-03-04 15:54:47 -0800

    🌻UPDATE: finished setup

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Box, Text } from '@chakra-ui/react'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+    return (
+        <Box>
+            <Text fontSize='32pt'>
+                Test
+            </Text>
+        </Box>
+    )
+}
+
+export default Home
