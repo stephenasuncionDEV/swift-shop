95e6968882b7d8eed935b673ed6e507d595735f2
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-03-05 17:18:11 -0800

    🌻UPDATE: clean ups

--- src/components/pages/landing/index.js
+++ src/components/pages/landing/index.js
@@ -1,142 +1,72 @@
-import { Box, Text,Image,Flex,Heading,Spacer,Center, VStack, WrapItem, Wrap} from '@chakra-ui/react'
+import { Box, Text, Image, Flex, Heading, Spacer, Center, VStack, WrapItem, Wrap } from '@chakra-ui/react'
+
+const HotDealsList = [
+    {title: 'Item 1', desc: 'This is where the description goes!'},
+    {title: 'Item 2', desc: 'This is where the description goes!'},
+    {title: 'Item 3', desc: 'This is where the description goes!'},
+    {title: 'Item 4', desc: 'This is where the description goes!'},
+]
+
+const SiteInfoList = [
+    {heading: 'Heading 1', subHeading: '', desc: 'This is some description like i dont know what to write here. May be with the God grace i will know soon.This is some description like i dont know what to write here. May be with the God grace i will know soon. This is some description like i dont know what to write here. May be with the God grace i will know soon. This is some description like i dont know what to write here. May be with the God grace i will know soon.'},
+    {heading: 'Heading 2', subHeading: '', desc: 'This is some description like i dont know what to write here. May be with the God grace i will know soon.This is some description like i dont know what to write here. May be with the God grace i will know soon. This is some description like i dont know what to write here. May be with the God grace i will know soon. This is some description like i dont know what to write here. May be with the God grace i will know soon.'},
+]
 
 const LandingContents = () => {
     return ( 
-
-        <Flex m="10px" flexWrap="wrap" spacing='24px' bg='black' p="10px">
-
-            <Box mb="10px"  width="100%">
-                <Image 
-                height='500px'
-                width='100%'
-                objectFit='cover'
-                src = 'gibbresh.png' 
-                fallbackSrc = 'https://via.placeholder.com/500'/>
-            </Box>
-
-            <Wrap bgGradient="linear(to-t, blue.400, pink.700)" width="100%" padding={"10px"}>
-
-                <WrapItem width="100%" height="80px" p="10px">
-                    <Center
-                        width="100%"
-                        fontSize="5xl"
-                        fontWeight="extrabold"
-                    >
-                        HOT DEALS
-                    </Center>
-                </WrapItem>
-
-                <Spacer />
-
-                <WrapItem >
-                    <VStack p="10px" bg="black" borderRadius="10px">
+        <Flex mt="10px" flexWrap="wrap" px="24px" justifyContent='center'>
+            <Flex maxW='8xl' w='full' flexDir='column'>
+                <Box mb="10px">
+                    <Image 
+                        height='500px'
+                        width='100%'
+                        objectFit='cover'
+                        src = 'gibbresh.png' 
+                        fallbackSrc = 'https://via.placeholder.com/1920x525'
+                    />
+                </Box>
+                <Flex flexDir='column' mt='1em'>
+                    <Text fontSize="5xl" fontWeight="extrabold">
+                        Hot Deals
+                    </Text>
+                    <Text color='whiteAlpha.600'>
+                        Lorem erkemremrmermemremrmermerme asdadadasdasdsasdsasd
+                    </Text>
+                    <Flex justifyContent='space-evenly' mt='2em'>
+                        {HotDealsList.map((deal, idx) => (
+                            <VStack key={idx} p="10px" borderRadius="10px">
+                                <Image 
+                                    mb="10px"
+                                    objectFit='cover'
+                                    src = 'gibbresh.png' 
+                                    fallbackSrc = 'https://via.placeholder.com/300'
+                                />
+                                <Text>{deal.title}</Text>
+                                <Text>{deal.desc}</Text>
+                            </VStack>
+                        ))}
+                    </Flex>
+                </Flex>
+                {SiteInfoList.map((info, idx) => (
+                    <Box display="flex" width="100%" mt="10em" p="10px" flexDirection={idx === 0 ? "row-reverse" : "row"} key={idx}>
                         <Image 
+                            boxShadow='dark-lg' rounded='md'
                             mb="10px"
                             objectFit='cover'
                             src = 'gibbresh.png' 
                             fallbackSrc = 'https://via.placeholder.com/300'
                         />
-                        <Text>Item 1</Text>
-                        <Text>This is where the description goes!</Text>
-                    </VStack>
-             
-                </WrapItem>
-
-                <Spacer />
-                <WrapItem>
-
-                    <VStack p="10px" bg="black" borderRadius="10px">
-                        <Image 
-                            mb="10px"
-                            objectFit='cover'
-                            src = 'gibbresh.png' 
-                            fallbackSrc = 'https://via.placeholder.com/300'
-                        />
-                        <Text>Item 2</Text>
-                        <Text>This is where the description goes!</Text>
-                    </VStack>
-                </WrapItem>
-                <Spacer />
-                <WrapItem >
-                    <VStack p="10px" bg="black" borderRadius="10px">
-                        <Image 
-                            mb="10px"
-                            objectFit='cover'
-                            src = 'gibbresh.png' 
-                            fallbackSrc = 'https://via.placeholder.com/300'
-                        />
-                        <Text>Item 3</Text>
-                        <Text>This is where the description goes!</Text>
-                    </VStack>
-                </WrapItem>
-
-                <Spacer />
-                <WrapItem>
-                    <VStack p="10px" bg="black" borderRadius="10px">
-                        <Image 
-                            mb="10px"
-                            objectFit='cover'
-                            src = 'gibbresh.png' 
-                            fallbackSrc = 'https://via.placeholder.com/300'
-                        />
-                        <Text>Item 4</Text>
-                        <Text>This is where the description goes!</Text>
-                    </VStack>
-                </WrapItem>
-
-                <Spacer />
-            </Wrap>
-
-
-        <Box display="flex" width="100%" bg="pink.600"  mt="10px" p="10px" flexDirection="row-reverse">
-
-            <Image 
-                boxShadow='dark-lg' rounded='md'
-                mb="10px"
-                objectFit='cover'
-                src = 'gibbresh.png' 
-                fallbackSrc = 'https://via.placeholder.com/300'
-            />
-
-            <VStack  border='2px' borderColor='gray.200' bg="black" p="20px" m="20px">
-                <Heading >I'm a Heading</Heading>
-                <Text fontSize='2xl'>This is some description like i dont know what to write here. May be with the God grace i will know soon.</Text>
-                <Text>
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    
-                </Text>
-
-            </VStack>
-        </Box>
-
-        <Box display="flex" width="100%" bg="pink.600"  mt="10px" p="10px" >
-
-           <Image 
-                boxShadow='dark-lg' rounded='md'
-                mb="10px"
-                objectFit='cover'
-                src = 'gibbresh.png' 
-                fallbackSrc = 'https://via.placeholder.com/300'
-            />
-
-            <VStack border='2px' borderColor='gray.200' bg="black" p="20px" m="20px">
-                <Heading >I'm a Heading</Heading>
-                <Text fontSize='2xl'>This is some description like i dont know what to write here. May be with the God grace i will know soon.</Text>
-                <Text>
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    This is some description like i dont know what to write here. May be with the God grace i will know soon.
-                    
-                </Text>
-
-            </VStack>
-        </Box>
-
-
-
-    </Flex>
+                        <VStack p="20px" m="20px">
+                            <Heading>{info.heading}</Heading>
+                            <Text fontSize='2xl'>{info.subHeading}</Text>
+                            <Text>
+                                {info.desc} 
+                            </Text>
+                        </VStack>
+                    </Box>
+                ))}
+            </Flex>
+        </Flex>
     )
 }
 
