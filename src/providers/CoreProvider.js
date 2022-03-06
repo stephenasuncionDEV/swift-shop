57556636be3ce08af85b9ec981855eb15dd0055e
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-03-05 18:54:28 -0800

    🌻UPDATE: finished hot deals

--- src/providers/CoreProvider.js
+++ src/providers/CoreProvider.js
@@ -7,14 +7,32 @@ export const CoreContext = createContext({})
 export const useCore = () => useContext(CoreContext)
 
 export const CoreProvider = ({ children }) => {
-    
+    const [products, setProducts] = useState([]);
+    const [hotDeals, setHotDeals] = useState([]);
 
     useEffect(() => {
+        const getProducts = async () => {
 
+            // Get all products
+            const res = await commerce.products.list();
+            setProducts(res.data);
+
+            console.log(res.data[0])
+
+            // Get hot deals
+            let tempHotDeals = [];
+            for (let i = 0; i < 5; i++) {
+                tempHotDeals.push(res.data[Math.floor(Math.random() * res.data.length)]);
+            }
+            setHotDeals(tempHotDeals);
+
+        }
+        getProducts();
     }, [])
 
     const controllers = {
-        
+        products,
+        hotDeals
     }
 
     return (
